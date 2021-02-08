37b5111c9ced979930cf667d76abaacbd1014bf1
Dominic Barnes <dominic@dbarnes.info>
2021-02-08 14:57:56 -0800

    feat: add Value helpers for converting pointer to scalar, add time.Time support

--- pointers.go
+++ pointers.go
@@ -1,21 +1,69 @@
 package pointer
 
+import "time"
+
 func Int(v int) *int {
 	return &v
+}
+
+func IntValue(v *int) int {
+	if v != nil {
+		return *v
+	}
+	return 0
 }
 
 func Int64(v int64) *int64 {
 	return &v
 }
 
+func Int64Value(v *int64) int64 {
+	if v != nil {
+		return *v
+	}
+	return 0
+}
+
 func String(v string) *string {
 	return &v
+}
+
+func StringValue(v *string) string {
+	if v != nil {
+		return *v
+	}
+	return ""
 }
 
 func Float(v float64) *float64 {
 	return &v
 }
 
+func FloatValue(v *float64) float64 {
+	if v != nil {
+		return *v
+	}
+	return 0
+}
+
 func Bool(v bool) *bool {
 	return &v
+}
+
+func BoolValue(v *bool) bool {
+	if v != nil {
+		return *v
+	}
+	return false
+}
+
+func Time(v time.Time) *time.Time {
+	return &v
+}
+
+func TimeValue(v *time.Time) time.Time {
+	if v != nil {
+		return *v
+	}
+	return time.Time{}
 }
